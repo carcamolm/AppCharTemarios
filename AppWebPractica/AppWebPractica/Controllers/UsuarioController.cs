ac5450d6ac8dd0832f5b42edaa7d4ed952b1ae6c
Luz <carcamolm@gmail.com>
2025-05-09 09:49:37 -0500

    app web

---
+++ AppWebPractica/AppWebPractica/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using AppWebPractica.Data;
+using AppWebPractica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppWebPractica.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        // GET api/<controller>
+        public List<Usuario> Get()
+        {
+            return UsuarioData.Listar();
+        }
+
+        // GET api/<controller>/5
+        public List<Usuario> Get(string id)
+        {
+            return UsuarioData.Obtener(id);
+        }
+
+        // POST api/<controller>
+        public bool Post([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.registrarUsuario(oUsuario);
+        }
+
+        public bool Put([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.actualizarUsuario(oUsuario);
+        }
+        public bool Delete(string id)
+        {
+            return UsuarioData.eliminarUsuario(id);
+        }
+    }
+}
---
+++ AppWebPractica/AppWebPractica/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppWebPractica.WebApiApplication" Language="C#" %>
